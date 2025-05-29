54ed7f54c216c2e3a870ed70b09643d9dcb47a39
Ibrahima Sarr <kognary@gmail.com>
2025-05-29 14:07:38 +0000

    added tables 6

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { zoneRouter } from "./routers/zone";
 import { topicRouter } from "./routers/topic";
 import { noteRouter } from "./routers/note";
 
@@ -9,7 +9,7 @@ import { noteRouter } from "./routers/note";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  zone: zoneRouter,
   topic: topicRouter,
   note: noteRouter,
 });
